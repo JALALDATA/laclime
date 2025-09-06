82e2da8bbc56b901e43f8c77465d0a754652f01d
unknown <a.alaeddinejalal@example.com>
2025-09-06 22:27:24 +0100

    Update Navbar and fix dark mode + add Logo and ThemeToggle components

---
+++ Climat/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.css";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <span className="logo-primary">La</span>
+      <span className="logo-secondary">
+        Clime
+      </span>
+    </div>
+  );
+}
