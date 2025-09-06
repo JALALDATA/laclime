086b87423eb000b957af294a93fde4d08683ef5e
Jalal <alaeddine.jalal@accenture.com>
2025-09-06 16:07:43 +0100

    first commit

---
+++ Climat/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Navbar = ({theme, setTheme}) => {
+  return (
+    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+      
+      {/* Logo */}
+      <img 
+        src={theme === 'dark' ? assets.logo_dark : assets.logo} 
+        className='w-32 sm:w-40' 
+        alt='Logo'
+      />
+
+      {/* Navigation Links */}
+      <div className='text-gray-700 dark:text-white sm:text-sm max-sm:w-60
+                      max-sm:pl-10 max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen
+                      max-sm:h-full max-sm:flex-col max-sm:bg-blue-600 max-sm:text-white 
+                      max-sm:pt-20 flex sm:items-center gap-5 transition-all'>
+
+        <img src={assets.close_icon}  alt="" className='w-5 absolute right-4 top-4 sm:hidden'/>
+        <a href="#" className='sm:hover:border-b'>Home</a>
+        <a href="#services" className='sm:hover:border-b'>Services</a>
+        <a href="#our-work" className='sm:hover:border-b'>Our Work</a>
+        <a href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
+      </div>
+
+      {/* Connect Button (desktop only) */}
+      <div>
+        <a 
+          href="#contact-us" 
+          className='text-sm max-sm:hidden flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-200'
+        >
+          Connect 
+          {assets.arrow_icon && (
+            <img src={assets.arrow_icon} width={14} alt="Arrow" />
+          )}
+        </a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
